631cbedac111b23a56d468b9d73608605ddca669
sunil5058 <sunilsquare999@gmail.com>
2025-10-05 20:54:29 +0545

    creating html tag using js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const toDoList=[];
+const toDoList=['make dinner', 'add dishes'];
+
+for(let i=0;i<toDoList.length;i++){
+    const todo=toDoList[i];
+    const html=`<p>${todo}</p>`
+
+}
+
 
 function addToDo(){
    const inputElement= document.querySelector('.js-name-input');
    const name =inputElement.value
    toDoList.push(name);
    console.log(toDoList)
+
+   inputElement.value='';
+
+
+
+
 
 }
